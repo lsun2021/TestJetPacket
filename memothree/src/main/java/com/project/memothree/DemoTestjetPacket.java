b8c4407670b5b4960cb64cfa6dbb01cbcdfdb55f
wangxin <799578743@qq.com>
2021-02-18 15:45:08 +0800

    first commit

---
+++ memothree/src/main/java/com/project/memothree/DemoTestjetPacket.java
@@ -0,0 +1,19 @@
+package com.project.memothree;
+
+/**
+ * Author : memoThree
+ * Date   : 2/18/21
+ * Desc   :
+ */
+public class DemoTestjetPacket {
+
+    /**
+     * 计算输入值 * 2
+     *
+     * @param num 输入值
+     * @return 结果
+     */
+    public int Cal(int num) {
+        return num * 2;
+    }
+}
